46595136e4ceb7141036e5ca4ec465899673889f
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-05-01 16:44:53 +0300

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   instance_type = "t2.micro"
-  ami           = "ami-060a87cc8d5dd5a91"
+  ami           = "ami-057d3df51a1c53485"
   key_name = "ivan-ec2-west-2"
   vpc_security_group_ids = ["${var.VM_sec_group}"]
   subnet_id              = var.private_subnet_id
